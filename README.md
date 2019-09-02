388621b4c7263ae148c951f35a0a89071ab5b112
HemaSree1519 <khemasree96@gmail.com>
2019-09-02 23:15:29 +0530

    Initialize spring application

---
+++ README.md
@@ -0,0 +1,2 @@
+# Google Keep
+Google keep is an application that enables users to add notes into their individual account. It also allows users to update and delete their notes.
---
+++ src/main/java/com/googlekeep/googlekeep/GooglekeepApplication.java
@@ -0,0 +1,13 @@
+package com.googlekeep.googlekeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooglekeepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GooglekeepApplication.class, args);
+    }
+
+}
