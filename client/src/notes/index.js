7678bf58bf194e7c1c6d270c52189fce6ff1809a
HemaSree1519 <khemasree96@gmail.com>
2019-09-03 15:46:41 +0530

    Expand the input area when clicked to write note

--- client/src/notes/index.js
+++ client/src/notes/index.js
@@ -1,12 +1,42 @@
 import {Input} from "reactstrap";
 import React, {Component} from "react";
+import "./styles.css"
+import WriteNote from "./components/WriteNote";
 
 export default class Index extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isWritingNote: false,
+            writingNoteTitle: '',
+            writingNoteContent: ''
+        }
+    }
+
+    onWriteToggle = () => {
+            this.setState({isWritingNote: !this.state.isWritingNote, writingNoteTitle: '', writingNoteContent: ''})
+    };
+    onWriteNoteTitle = (writingNoteTitle) => {
+        this.setState({writingNoteTitle: writingNoteTitle.target.value})
+    };
+    onWriteNoteContent = (writeNoteContent) => {
+        this.setState({writingNoteContent: writeNoteContent.target.value})
+    };
     render() {
+        const props = {
+            writingNoteTitle: this.state.writingNoteTitle,
+            writingNoteContent: this.state.writingNoteContent,
+            onWriteNoteTitle: this.onWriteNoteTitle,
+            onWriteNoteContent: this.onWriteNoteContent,
+            onWriteToggle: this.onWriteToggle
+        };
         return (
             <div className="dash-board">
                 <div className="write-note">
-                    <Input className="note-input" type="text" placeholder="Write a note..."/>
+                    {!this.state.isWritingNote ?
+                        <Input className="note-input" type="text" placeholder="Write a note..." onClick={() => {
+                            this.onWriteToggle();
+                        }}/> : <WriteNote childProps={props}/>}
                 </div>
             </div>
         )
