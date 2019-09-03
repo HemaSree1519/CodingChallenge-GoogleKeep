78e255532fb0af7ce9a59d4fa43a330e4a782111
HemaSree1519 <khemasree96@gmail.com>
2019-09-03 21:52:18 +0530

    Add failing test for onclick of close button

--- client/src/notes/__tests__/WriteNote.test.js
+++ client/src/notes/__tests__/WriteNote.test.js
@@ -3,11 +3,18 @@ import {shallow} from 'enzyme'
 import WriteNote from "../components/WriteNote";
 
 describe('WriteNotes test', () => {
-    const props = {
-        writingNoteTitle: "",
-        writingNoteContent: "",
-    };
-    const wrapper = shallow(<WriteNote childProps={props}/>);
+    let props;
+    let wrapper;
+    beforeEach(() => {
+        props = {
+            writingNoteTitle: '',
+            writingNoteContent: '',
+            onCloseNewNote: jest.fn(),
+            onWriteNoteContent: jest.fn(),
+            onWriteNoteTitle: jest.fn(),
+        };
+        wrapper = shallow(<WriteNote childProps={props}/>);
+    });
     it('should render 1 <Card />', () => {
         expect(wrapper.find('Card').length).toEqual(1);
     });
@@ -20,6 +27,17 @@ describe('WriteNotes test', () => {
     it('should render 1 <Button />', () => {
         expect(wrapper.find('Button').length).toEqual(1);
     });
-
+    it("should call title writing function when value changed", () => {
+        wrapper.find('Input[name="title"]').simulate('change', {value: 'title'});
+        expect(props.onWriteNoteTitle).toHaveBeenCalledTimes(1);
+    });
+    it("should call content writing function when value changed", () => {
+        wrapper.find('Input[name="content"]').simulate('change', {value: 'content'});
+        expect(props.onWriteNoteContent).toHaveBeenCalledTimes(1);
+    });
+    it("should call close function upon close button pressed", () => {
+        wrapper.find('Button[name="close"]').simulate('click');
+        expect(props.onCloseNewNote).toHaveBeenCalledTimes(1);
+    });
 
 });
