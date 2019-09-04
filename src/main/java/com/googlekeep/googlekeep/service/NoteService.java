6610575f354c651806dd0f087d28f585f19d6715
HemaSree1519 <khemasree96@gmail.com>
2019-09-04 22:24:38 +0530

    Add get notes function in note service

--- src/main/java/com/googlekeep/googlekeep/service/NoteService.java
+++ src/main/java/com/googlekeep/googlekeep/service/NoteService.java
@@ -5,11 +5,17 @@ import com.googlekeep.googlekeep.repository.NoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class NoteService {
     @Autowired
     NoteRepository noteRepository;
 
+    List<Note> getNotes(String email) {
+        return noteRepository.findByEmail(email);
+
+    }
     Note addNote(Note note) {
         return noteRepository.save(note);
     }
