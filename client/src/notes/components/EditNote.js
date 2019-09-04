ce84204565c09ababe9b5fb094756c7a5a7c47e2
HemaSree1519 <khemasree96@gmail.com>
2019-09-04 07:46:38 +0530

    Pass the test by adding the onclick functions in edit note

--- client/src/notes/components/EditNote.js
+++ client/src/notes/components/EditNote.js
@@ -4,31 +4,33 @@ import {Button, Input, Label, Modal, ModalBody, ModalFooter, ModalHeader} from "
 export default class EditNote extends Component {
     render() {
         return (
-            <Modal >
+            <Modal isOpen={this.props.childProps.isEditingNote} toggle={this.props.childProps.setEditState}>
                 <ModalHeader>
                     <Input type="textarea"
                            name="title"
                            className="note-card-title-input"
-                           value={""}
-                           onChange={""}
+                           value={this.props.childProps.editingNoteTitle}
+                           onChange={this.props.childProps.onEditNoteTitle}
                     />
                 </ModalHeader>
                 <ModalBody>
                     <Input type="textarea"
                            name="content"
                            className="note-card-content-input"
-                           value={""}
-                           onChange={""}
+                           value={this.props.childProps.editingNoteContent}
+                           onChange={this.props.childProps.onEditNoteContent}
                            rows={5}/>
                     <br/>
                     <div className="footer-container">
                         <Label className="footer-edited-time">
-                            Edited </Label>
+                            Edited</Label>
                     </div>
                 </ModalBody>
                 <ModalFooter className="modal-footer">
-                    <Button name="delete" className="footer-button">Delete</Button>{'  '}
-                    <Button name="close" className="footer-button">Close</Button>
+                    <Button name="delete" className="footer-button"
+                            onClick={this.props.childProps.onDelete}>Delete</Button>{'  '}
+                    <Button name="close" className="footer-button"
+                            onClick={this.props.childProps.onUpdateNote}>Close</Button>
                 </ModalFooter>
             </Modal>
         )
