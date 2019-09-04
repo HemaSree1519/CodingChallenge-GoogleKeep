2b39e379f9fde5daa7a56b47d3a700c243822b25
HemaSree1519 <khemasree96@gmail.com>
2019-09-04 07:39:05 +0530

    Add edit note with header, body and footer then pass the test

--- client/src/notes/__tests__/EditNote.test.js
+++ client/src/notes/__tests__/EditNote.test.js
@@ -10,16 +10,15 @@ describe('EditNote', () => {
     it('should render an edit note modal header', () => {
         expect(wrapper.find('Modal').length).toEqual(1);
         expect(wrapper.find('ModalHeader').length).toEqual(1);
-        expect(wrapper.find('Input').length).toEqual(1);
+        expect(wrapper.find('Input').length).toEqual(2);
     });
     it('should render an edit note modal body',()=>{
         expect(wrapper.find('ModalBody').length).toEqual(1);
-        expect(wrapper.find('Input').length).toEqual(1);
         expect(wrapper.find('div').length).toEqual(1);
         expect(wrapper.find('Label').length).toEqual(1);
     });
     it('should render an edit note modal footer',()=>{
-        expect(wrapper.find('ModalFoote').length).toEqual(1);
+        expect(wrapper.find('ModalFooter').length).toEqual(1);
         expect(wrapper.find('Button').length).toEqual(2);
     })
 });
--- client/src/notes/components/EditNote.js
+++ client/src/notes/components/EditNote.js
@@ -1,18 +1,35 @@
 import React, {Component} from "react";
-import {Input, Modal, ModalHeader} from "reactstrap";
+import {Button, Input, Label, Modal, ModalBody, ModalFooter, ModalHeader} from "reactstrap";
 
 export default class EditNote extends Component {
     render() {
         return (
-          <Modal>
-              <ModalHeader>
-                  <Input type="textarea"
-                         name="title"
-                         className="note-card-title-input"
-                         value={this.props.childProps.editingNoteTitle}
-                         onChange={this.props.childProps.onEditNoteTitle}
-                  />
-              </ModalHeader>
+            <Modal >
+                <ModalHeader>
+                    <Input type="textarea"
+                           name="title"
+                           className="note-card-title-input"
+                           value={""}
+                           onChange={""}
+                    />
+                </ModalHeader>
+                <ModalBody>
+                    <Input type="textarea"
+                           name="content"
+                           className="note-card-content-input"
+                           value={""}
+                           onChange={""}
+                           rows={5}/>
+                    <br/>
+                    <div className="footer-container">
+                        <Label className="footer-edited-time">
+                            Edited </Label>
+                    </div>
+                </ModalBody>
+                <ModalFooter className="modal-footer">
+                    <Button name="delete" className="footer-button">Delete</Button>{'  '}
+                    <Button name="close" className="footer-button">Close</Button>
+                </ModalFooter>
             </Modal>
         )
     }
