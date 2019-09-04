1ad0ae5d753771d4be2919106a26c44b3edad7cc
HemaSree1519 <khemasree96@gmail.com>
2019-09-04 16:42:09 +0530

    Add test expecting login function to be called when the button is clicked

--- client/src/containers/login/__tests__/index.test.js
+++ client/src/containers/login/__tests__/index.test.js
@@ -3,12 +3,24 @@ import {shallow} from 'enzyme'
 import Index from "../../login";
 
 describe('Login',()=>{
-    const wrapper = shallow(<Index/>);
+    let wrapper;
+    let instance;
+    beforeEach(()=>{
+        wrapper = shallow(<Index/>);
+        instance = wrapper.instance();
+    });
    it('should render a login form',()=>{
        expect(wrapper.find('Form').length).toEqual(1);
        expect(wrapper.find('Col').length).toEqual(2);
        expect(wrapper.find('Input').length).toEqual(2);
        expect(wrapper.find('FormGroup').length).toEqual(2);
        expect(wrapper.find('Button').length).toEqual(1);
-   })
+   });
+    it("should Login with given details", () => {
+        instance.handleLogin = jest.fn(instance.handleLogin);
+        wrapper.find('Input[name="email"]').simulate('change', {target: {value: 'Tester'}});
+        wrapper.find('Input[name="password"]').simulate('change', {target: {value: 'password'}});
+        wrapper.find('Button').simulate('click');
+        wrapper(instance.handleLogin).toHaveBeenCalled();
+    });
 });
