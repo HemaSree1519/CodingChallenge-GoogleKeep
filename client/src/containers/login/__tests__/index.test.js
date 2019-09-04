d9316bfac5375bf39fd8d70176f01e1caf1637f0
HemaSree1519 <khemasree96@gmail.com>
2019-09-04 16:33:46 +0530

    Add test for login form with two columns to enter email and password

--- client/src/containers/login/__tests__/index.test.js
+++ client/src/containers/login/__tests__/index.test.js
@@ -6,5 +6,9 @@ describe('Login',()=>{
     const wrapper = shallow(<Index/>);
    it('should render a login form',()=>{
        expect(wrapper.find('Form').length).toEqual(1);
+       expect(wrapper.find('Col').length).toEqual(2);
+       expect(wrapper.find('Input').length).toEqual(2);
+       expect(wrapper.find('FormGroup').length).toEqual(2);
+       expect(wrapper.find('Button').length).toEqual(1);
    })
 });
