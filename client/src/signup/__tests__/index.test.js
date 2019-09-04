8e37f6e03032eb2769934be172f80b407494c3bd
HemaSree1519 <khemasree96@gmail.com>
2019-09-04 13:02:00 +0530

    Add failing test for signup with given details when signup button is clicked

--- client/src/signup/__tests__/index.test.js
+++ client/src/signup/__tests__/index.test.js
@@ -4,13 +4,24 @@ import Index from "../../signup";
 
 describe('SingUp', ()=>{
     let wrapper;
+    let instance;
     beforeEach(()=>{
         wrapper = shallow(<Index />);
+        instance = wrapper.instance();
     });
     it('should render signup form',()=>{
         expect(wrapper.find('Form').length).toEqual(1);
         expect(wrapper.find('Col').length).toEqual(3);
         expect(wrapper.find('FormGroup').length).toEqual(3);
         expect(wrapper.find('Button').length).toEqual(1);
-    })
+    }) ;
+    it("should signUp with given details", () => {
+        instance.handleSignUp = jest.fn(instance.handleSignUp);
+        wrapper.find('Input[name="email"]').simulate('change', {target: {value: 'Tester'}});
+        wrapper.find('Input[name="password"]').simulate('change', {target: {value: 'password'}});
+        wrapper.find('Input[name="rePassword"]').simulate('change', {target: {value: 'password'}});
+        wrapper.find('Button').simulate('click');
+        expect(areMatchingPasswords).toHaveBeenCalledTimes(1);
+        expect(instance.handleSignUp).toHaveBeenCalled();
+    });
 });
