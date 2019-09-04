fa9b658c7f67ab72bb47cf282037b6db737f8c65
HemaSree1519 <khemasree96@gmail.com>
2019-09-04 21:38:28 +0530

    Add test for duplicate entry exception

---
+++ src/test/java/com/googlekeep/googlekeep/exception/DuplicateEntryExceptionUnitTest.java
@@ -0,0 +1,11 @@
+package com.googlekeep.googlekeep.exception;
+
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class DuplicateEntryExceptionUnitTest {
+
+    @Test
+    public void when_given_parameters_it_return_expected_message(){
+        assertEquals("User already exist with email : 'duplicate@email.com'",new DuplicateEntryException("User","email","duplicate@email.com").getMessage());
+    }
+}
