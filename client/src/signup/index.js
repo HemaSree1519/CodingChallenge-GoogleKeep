1ffbc6b91c5944ba2e5aede2395c6885e4c3fbe8
HemaSree1519 <khemasree96@gmail.com>
2019-09-04 12:51:35 +0530

    Pass the signup test by adding signup with simple form

---
+++ client/src/signup/index.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import {Button, Col, Form, FormGroup, Input} from "reactstrap";
+
+let userEmail = '';
+let userPassword = '';
+let userReEnteredPassword = '';
+export default class Index extends Component {
+
+    render() {
+        return (
+            <Form>
+            </Form>
+        );
+    }
+}
