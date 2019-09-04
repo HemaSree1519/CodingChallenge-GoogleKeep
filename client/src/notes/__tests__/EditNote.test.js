d39c7049dc47885f8d1c57fae4cb1ae18db59482
HemaSree1519 <khemasree96@gmail.com>
2019-09-04 07:34:22 +0530

    Add failing test for existance of edit note with header, body and footer

--- client/src/notes/__tests__/EditNote.test.js
+++ client/src/notes/__tests__/EditNote.test.js
@@ -7,7 +7,19 @@ describe('EditNote', () => {
     beforeEach(() => {
         wrapper = shallow(<EditNote/>);
     });
-    it('should render a edit note modal', () => {
-        expect(wrapper.find('Modal').length).toEqual(1)
+    it('should render an edit note modal header', () => {
+        expect(wrapper.find('Modal').length).toEqual(1);
+        expect(wrapper.find('ModalHeader').length).toEqual(1);
+        expect(wrapper.find('Input').length).toEqual(1);
+    });
+    it('should render an edit note modal body',()=>{
+        expect(wrapper.find('ModalBody').length).toEqual(1);
+        expect(wrapper.find('Input').length).toEqual(1);
+        expect(wrapper.find('div').length).toEqual(1);
+        expect(wrapper.find('Label').length).toEqual(1);
+    });
+    it('should render an edit note modal footer',()=>{
+        expect(wrapper.find('ModalFoote').length).toEqual(1);
+        expect(wrapper.find('Button').length).toEqual(2);
     })
 });
--- client/src/notes/components/EditNote.js
+++ client/src/notes/components/EditNote.js
@@ -1,10 +1,18 @@
 import React, {Component} from "react";
-import {Modal} from "reactstrap";
+import {Input, Modal, ModalHeader} from "reactstrap";
 
 export default class EditNote extends Component {
     render() {
         return (
           <Modal>
+              <ModalHeader>
+                  <Input type="textarea"
+                         name="title"
+                         className="note-card-title-input"
+                         value={this.props.childProps.editingNoteTitle}
+                         onChange={this.props.childProps.onEditNoteTitle}
+                  />
+              </ModalHeader>
             </Modal>
         )
     }
