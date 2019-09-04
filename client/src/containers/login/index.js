da21ca2a9a2f3b76de654923a43ccc5b1e0a2f5b
HemaSree1519 <khemasree96@gmail.com>
2019-09-04 16:31:55 +0530

    Pass login test by creating a simple form in login

---
+++ client/src/containers/login/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+import {Form} from "reactstrap";
+
+export default class Index extends Component {
+
+
+    render() {
+        return (
+            <Form>
+            </Form>
+        );
+    }
+}
