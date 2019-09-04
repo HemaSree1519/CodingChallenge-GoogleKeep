763ee44500b1f638b61761a03761f30ee18917d0
HemaSree1519 <khemasree96@gmail.com>
2019-09-04 21:10:43 +0530

    Add test for finding user by email adn find all the users with TestEntityManager

--- src/test/java/com/googlekeep/googlekeep/repository/UserRepositoryUnitTest.java
+++ src/test/java/com/googlekeep/googlekeep/repository/UserRepositoryUnitTest.java
@@ -29,4 +29,24 @@ public class UserRepositoryUnitTest {
         user = entityManager.persistAndFlush(user);
         assertThat(userRepository.findById(user.getEmail()).isPresent()).isTrue();
     }
+
+    @Test
+    public void it_should_find_user_byId() {
+        user = entityManager.persistAndFlush(user);
+        assertThat(userRepository.findById(user.getEmail()).get()).isEqualTo(user);
+    }
+
+    @Test
+    public void it_should_find_all_users() {
+        User user2 = new User();
+        user2.setEmail("UnitTester2@gmail.com");
+        user2.setUserName("test_user2");
+        user2.setPassword("password2");
+        user = entityManager.persistAndFlush(user);
+        user2 = entityManager.persistAndFlush(user2);
+        System.out.println(userRepository.findAll());
+        assertThat(userRepository.findAll().get(0)).isEqualTo(user);
+        assertThat(userRepository.findAll().get(1)).isEqualTo(user2);
+    }
+
 }
