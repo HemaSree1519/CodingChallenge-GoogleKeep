3f75c7f7044855e2b2be295383461a7b9e89dbe6
HemaSree1519 <khemasree96@gmail.com>
2019-09-04 12:56:23 +0530

    Pass the test by creating signup form with three columns

--- client/src/signup/index.js
+++ client/src/signup/index.js
@@ -9,7 +9,24 @@ export default class Index extends Component {
     render() {
         return (
             <Form>
-
+                <Col>
+                    <FormGroup>
+                        <Input type="email" name="email" id="exampleEmail" placeholder="example@gmail.com"
+                        />
+                    </FormGroup>
+                </Col>
+                <Col>
+                    <FormGroup>
+                        <Input type="password" name="password" placeholder="Password"/>
+                    </FormGroup>
+                </Col>
+                <Col>
+                    <FormGroup>
+                        <Input type="password" name="rePassword" placeholder="Re-Enter Password"
+                        />
+                    </FormGroup>
+                </Col>
+                <Button>SignUp</Button>
             </Form>
         );
     }
