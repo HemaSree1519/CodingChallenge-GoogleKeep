720e1a8b1684957f797c8704434c454e0e36605b
HemaSree1519 <khemasree96@gmail.com>
2019-09-04 19:20:16 +0530

    Add functions to update login state

--- client/src/containers/login/index.js
+++ client/src/containers/login/index.js
@@ -1,23 +1,47 @@
 import React, {Component} from "react";
+import "./styles.css";
 import {Button, Col, Form, FormGroup, Input} from "reactstrap";
+import {setUser} from "../../session/UserSession";
 
 let userEmail = '';
 let userPassword = '';
 export default class Index extends Component {
 
-    handleLogin = ()=> {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isError: false,
+            errorMessage: ''
+        }
+    }
+
+    handleLogin = () => {
+        setUser(userEmail);
+        userEmail = '';
+        userPassword = '';
         this.props.history.push('/notes');
+
+    };
+    setErrorState = (flag, message) => {
+        this.setState({
+            isError: flag,
+            errorMessage: message
+        });
     };
     onEmail = (email) => {
+        this.setErrorState(false, '');
         userEmail = email.target.value;
     };
 
     onPassword = (password) => {
+        this.setErrorState(false, '');
         userPassword = password.target.value;
     };
+
     render() {
         return (
             <Form>
+                {this.state.isError && <p className="error">{this.state.errorMessage}</p>}
                 <Col>
                     <FormGroup>
                         <Input type="email" name="email" placeholder="example@gmail.com" onChange={this.onEmail}/>
